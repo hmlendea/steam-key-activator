170597580d982d416e543540efa6870f5e4cdbfc
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-09-01 22:49:45 +0300

    Scaffolding

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SteamKeyActivator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
