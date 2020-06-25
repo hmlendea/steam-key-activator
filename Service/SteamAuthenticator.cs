2bf7c9d8865738497a93d8fa3b2069f6e920991b
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-06-25 15:43:23 +0300

    Fixed session validation

--- Service/SteamAuthenticator.cs
+++ Service/SteamAuthenticator.cs
@@ -115,7 +115,7 @@ namespace SteamKeyActivator.Service
 
         void ValidateCurrentSession()
         {
-            By accountPulldownSelector = By.XPath("//a[contains(@class,'online')]");
+            By accountPulldownSelector = By.XPath("//span[contains(@class,'online')]");
 
             string currentUsername = webProcessor.GetText(accountPulldownSelector).Trim();
 
